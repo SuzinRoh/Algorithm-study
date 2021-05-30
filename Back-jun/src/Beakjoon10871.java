39892849e8eb0bc52db94773365aa3eb0c0337ea
SUZIN <suuz.hha@gmail.com>
2021-05-30 22:35:39 +0900

    sunday

---
+++ Back-jun/src/Beakjoon10871.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Beakjoon10871 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine().trim());
+		int x = Integer.parseInt(br.readLine().trim());
+		StringTokenizer st;
+		
+		
+	}
+	
+
+}
+ㅌㅈ
