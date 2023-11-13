aa0f7251d5e9cbd42154d76f32e20e680356ebec
SUJIN ROH <suuz.hha@gmail.com>
2023-11-13 21:38:30 +0900

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String [] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    Integer year = sc.nextInt();
+
+    boolean leapYear = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+
+    System.out.print(leapYear ? 1 : 0);
+
+  }
+}
